5eee369882fe2753081071d7769e161fb02ad7f0
Alberto Olla <webservicesolla@gmail.com>
2016-08-18 17:11:55 +0200

    Updated text

--- esercizi-variabili/gatti-in-fila.js
+++ esercizi-variabili/gatti-in-fila.js
@@ -8,6 +8,7 @@
    - Nel caso l'ultima fila risulti incompleta, il numero di gatti mancanti
   Esempio
     Input: numero di gatti = 44, gatti per fila = 6
-    Outputnumero file = 8, gatti mancanti = 4
+    Output: numero di file = 8, gatti mancanti = 4
+  Hint: Se vuoi usare l'arrotondamento ti consiglio di non usare Math.round (arrotonda all'intero superiore solo da .5 in su) ma utilizza Math.ceil (arrotonda sempre all'intero superiore).
   http://www.imparareaprogrammare.it
 */
