63e757a3ae801aeca8240ff58d333946a5ce68ef
Alberto Olla <AlbertoOlla@users.noreply.github.com>
2017-02-20 16:41:11 +0100

    Update la-calcolatrice.js

--- esercizi-condizioni/la-calcolatrice.js
+++ esercizi-condizioni/la-calcolatrice.js
@@ -1,6 +1,6 @@
 /*
   La calcolatrice
-  Scrivi un programma che prenda tr input:
+  Scrivi un programma che prenda tre input:
     - Due numeri
     - Una stringha che identifichi l'operazione da eseguire, es: "somma"
   Le operazioni supportate sono le suguenti:
