21ad795f5c5cdc5c35eda3547b6e51bee555e042
C35K02080 <114149724+C35K02080@users.noreply.github.com>
2022-09-24 17:24:04 +0200

    esercizio eseguito anche con la variante

--- esercizi-condizioni/fai-il-professore.js
+++ esercizi-condizioni/fai-il-professore.js
@@ -31,3 +31,52 @@ if (v < 18)
   console.log('ottimo')
   else
   console.log('eccellente');
+
+/*
+istruzione con la variante switch-case che in questo caso sembra più macchinosa
+*/
+
+switch (v) {
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+  case 7:
+  case 8:
+  case 9:
+  case 10:
+  case 11:
+  case 12:
+  case 13:
+  case 14:
+  case 15:
+  case 16:
+  case 17:
+console.log('insufficiente')
+    break;
+  case 18:
+  case 19:
+  case 20:
+console.log('sufficiente')
+    break;
+  case 21:
+  case 22:
+  case 23:
+console.log('buono')
+    break;
+  case 24:
+  case 25:
+  case 26:
+console.log('distinto')
+    break;
+  case 27:
+  case 28:
+  case 29:
+console.log('ottimo')
+    break;
+  default:
+console.log('eccellente')
+    break;
+};
