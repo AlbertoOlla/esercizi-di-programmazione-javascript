7ea68826ccfc2bff020198e254b097969a9c343b
C35K02080 <114149724+C35K02080@users.noreply.github.com>
2022-09-25 08:46:52 +0200

    esercizio eseguito anche con variante

--- esercizi-condizioni/che-giorno-oggi.js
+++ esercizi-condizioni/che-giorno-oggi.js
@@ -22,3 +22,72 @@
 
   http://www.imparareaprogrammare.it
 */
+
+var giorno = 7
+switch (giorno) {
+  case 1:
+    console.log('lunedì')
+    break;
+  case 2:
+    console.log('martedì')
+    break;
+  case 3:
+    console.log('mercoledì')
+    break;
+  case 4:
+    console.log('giovedì')
+    break;
+  case 5:
+    console.log('venerdì');
+    break;
+  case 6:
+    console.log('sabato')
+    break;
+  case 7:
+    console.log('domenica')
+    break;
+  default:
+    console.log('Errore! Giorno della settimana non valido!')
+}
+
+var mese= 9
+switch (mese) {
+  case 1:
+    console.log('gennaio')
+    break;
+  case 2:
+    console.log('febbraio')
+    break;
+  case 3:
+    console.log('marzo')
+    break;
+  case 4:
+    console.log('aprile')
+    break;
+  case 5:
+    console.log('maggio')
+    break;
+  case 6:
+    console.log('giugno')
+    break;
+  case 7:
+    console.log('luglio')
+    break;
+  case 8:
+    console.log('agosto')
+    break;
+  case 9:
+    console.log('settembre')
+    break;
+  case 10:
+    console.log('ottobre')
+    break;
+  case 11:
+    console.log('novembre')
+    break;
+  case 12:
+    console.log('dicembre')
+    break;
+  default:
+    console.log('Errore! Questo mese non esiste!')
+}
