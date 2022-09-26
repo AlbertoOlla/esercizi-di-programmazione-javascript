8e7372a295292aca5c229adb64ecbcadf61a0da5
C35K02080 <114149724+C35K02080@users.noreply.github.com>
2022-09-26 15:54:30 +0200

    esercizio eseguito

--- esercizi-condizioni/caldo-o-freddo.js
+++ esercizi-condizioni/caldo-o-freddo.js
@@ -9,3 +9,50 @@
 
   http://www.imparareaprogrammare.it
 */
+var lun = 10, mar = 15, mer = 17, gio = 16, ven = 9, sab = 18, dom =20;
+var hot = Math.max(lun, mar, mer, gio, ven, sab, dom);
+var cold = Math.min(lun, mar, mer, gio, ven, sab, dom);
+switch (hot) {
+  case lun:
+    console.log(`La giornata più calda è stata lunedì con ${lun} gradi`)
+    break;
+    case mar:
+      console.log(`La giornata più calda è stata martedì con ${mar} gradi`)
+    break;
+    case mer:
+      console.log(`La giornata più calda è stata mercoledì con ${mer} gradi`)
+    break;
+    case gio:
+      console.log(`La giornata più calda è stata giovedì con ${gio} gradi`)
+    break;
+    case ven:
+      console.log(`La giornata più calda è stata venerdì con ${ven} gradi`)
+    break;
+    case sab:
+      console.log(`La giornata più calda è stata sabato con ${sab} gradi`)
+    break;
+    case dom:
+      console.log(`La giornata più calda è stata domenica con ${dom} gradi`)
+};
+switch (cold) {
+  case lun:
+    console.log(`La giornata più fredda è stata lunedì con ${lun} gradi`)
+    break;
+    case mar:
+      console.log(`La giornata più fredda è stata martedì con ${mar} gradi`)
+    break;
+    case mer:
+      console.log(`La giornata più fredda è stata mercoledì con ${mer} gradi`)
+    break;
+    case gio:
+      console.log(`La giornata più fredda è stata giovedì con ${gio} gradi`)
+    break;
+    case ven:
+      console.log(`La giornata più fredda è stata venerdì con ${ven} gradi`)
+    break;
+    case sab:
+      console.log(`La giornata più fredda è stata sabato con ${sab} gradi`)
+    break;
+    case dom:
+      console.log(`La giornata più fredda è stata domenica con ${dom} gradi`)
+};
